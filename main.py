003bbe72d10c69b7a12f86cea090214f46209a42
Jeff Rayner <jeff.rfsimoes@senacsp.edu.br>
2026-01-28 11:57:08 -0300

    geração de QrCode

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUi
 from pyshorteners import Shortener
 from PyQt5.QtCore import pyqtSlot
+import qrcode
+from os import path
 
 def CREATE_SHORT_URL(url):
     link = Shortener()
     return link.tinyurl.short(url)
+
+def CREATE_QRCODE(link):
+    img = qrcode.make(link)
+    img.save("qrcode.png")
+
+def GET_LOCAL_PATH(myPath):
+    return f''
+
+
 
 
 class QrCodeBuilder(QMainWindow):
@@ -24,12 +36,33 @@ class QrCodeBuilder(QMainWindow):
         valor = self.getURL()
         if valor:
             url = CREATE_SHORT_URL(valor)
+            CREATE_QRCODE(url)
             self.setURLCurta(url)
+            self.img.setPixmap(QPixmap("qrcode.png"))
+            self.btnSalvar.setEnabled(True)
             
         else:
             self.showMessage("Errou", "Voce esquece de alguma coisa?")
 
-    
+    @pyqtSlot()
+    def on_btnSalvar_clicked(self):
+        self.salvar()
+
+    def salvar(self):
+        nomeArquivo, _ = QFileDialog.getSaveFileName(self, "Salvar Imagem")
+        if nomeArquivo:
+            caminho = path.dirname(nomeArquivo)
+            nome = nomeArquivo.removeprefix(caminho)
+
+            # ler a foto do QRCODE
+            with open("qrcode.png", "rb") as fotoQrcode:
+                dadosQrcode = fotoQrcode.read()
+
+            # salvar a foto aonde o user escolheu
+            with open(caminho+f"{nome}.png", "wb") as foto:
+                foto.write(dadosQrcode)
+            
+
     def showMessage(self, title, message):
         QMessageBox.information(self, title, message)
 
